733c84905f3d020d777ab3bfd9530950fd4aa9e2
Charles Aroma <charlesaroma9@gmail.com>
2025-01-03 00:02:20 +0300

    tech scroll section padding and responsiveness

--- src/pages/1homePage/6techScrollSection.jsx
+++ src/pages/1homePage/6techScrollSection.jsx
@@ -18,34 +18,35 @@ import {
   SiKotlin,
 } from 'react-icons/si';
 
+// eslint-disable-next-line react/prop-types
 const TechIcon = ({ icon, name }) => (
   <motion.div
-    className="flex flex-col items-center justify-center py-5 lg:py-10 w-24 h-24 md:w-32 md:h-32 lg:w-40 lg:h-40 bg-white shadow-lg rounded-full border-4 border-gray-200 transform hover:scale-110 transition-all duration-300"
+    className="flex flex-col items-center justify-center py-4 lg:py-10 w-28 h-28 md:w-32 md:h-32 lg:w-40 lg:h-40 bg-white shadow-lg rounded-full border-4 border-gray-200 transform hover:scale-110 transition-all duration-300"
   >
     <div className="flex items-center justify-center w-full h-full">{icon}</div>
-    <p className="text-gray-800 text-xs sm:text-sm md:text-base lg:text-lg font-semibold mt-1">{name}</p>
+    <p className="text-gray-800 text-sm md:text-base lg:text-lg font-semibold mt-1">{name}</p>
   </motion.div>
 );
 
 const TechScrollSection = () => {
   const containerRef = useRef(null);
-  const scrollSpeed = 1.2; // Adjust speed as necessary
+  const scrollSpeed = 0.8; // Adjust speed as necessary
 
   const technologies = useMemo(
     () => [
-      { name: 'React', icon: <FaReact className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-blue-500" /> },
-      { name: 'Node.js', icon: <FaNodeJs className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-green-500" /> },
-      { name: 'TailwindCSS', icon: <SiTailwindcss className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-teal-400" /> },
-      { name: 'MongoDB', icon: <SiMongodb className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-green-600" /> },
-      { name: 'JavaScript', icon: <FaJsSquare className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-yellow-500" /> },
-      { name: 'HTML5', icon: <FaHtml5 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-orange-500" /> },
-      { name: 'CSS3', icon: <FaCss3Alt className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-blue-600" /> },
-      { name: 'Angular', icon: <FaAngular className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-red-500" /> },
-      { name: 'Python', icon: <FaPython className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-green-500" /> },
-      { name: 'Java', icon: <FaJava className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-orange-600" /> },
-      { name: 'Next.js', icon: <SiNextdotjs className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-gray-800" /> },
-      { name: 'Flutter', icon: <SiFlutter className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-blue-400" /> },
-      { name: 'Kotlin', icon: <SiKotlin className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-purple-500" /> },
+      { name: 'React', icon: <FaReact className="text-4xl md:text-6xl lg:text-7xl text-blue-500" /> },
+      { name: 'Node.js', icon: <FaNodeJs className="text-4xl md:text-6xl lg:text-7xl text-green-500" /> },
+      { name: 'TailwindCSS', icon: <SiTailwindcss className="text-4xl md:text-6xl lg:text-7xl text-teal-400" /> },
+      { name: 'MongoDB', icon: <SiMongodb className="text-4xl md:text-6xl lg:text-7xl text-green-600" /> },
+      { name: 'JavaScript', icon: <FaJsSquare className="text-4xl md:text-6xl lg:text-7xl text-yellow-500" /> },
+      { name: 'HTML5', icon: <FaHtml5 className="text-4xl md:text-6xl lg:text-7xl text-orange-500" /> },
+      { name: 'CSS3', icon: <FaCss3Alt className="text-4xl md:text-6xl lg:text-7xl text-blue-600" /> },
+      { name: 'Angular', icon: <FaAngular className="text-4xl md:text-6xl lg:text-7xl text-red-500" /> },
+      { name: 'Python', icon: <FaPython className="text-4xl md:text-6xl lg:text-7xl text-green-500" /> },
+      { name: 'Java', icon: <FaJava className="text-4xl md:text-6xl lg:text-7xl text-orange-600" /> },
+      { name: 'Next.js', icon: <SiNextdotjs className="text-4xl md:text-6xl lg:text-7xl text-gray-800" /> },
+      { name: 'Flutter', icon: <SiFlutter className="text-4xl md:text-6xl lg:text-7xl text-blue-400" /> },
+      { name: 'Kotlin', icon: <SiKotlin className="text-4xl md:text-6xl lg:text-7xl text-purple-500" /> },
     ],
     []
   );
@@ -70,20 +71,20 @@ const TechScrollSection = () => {
   }, [scrollSpeed]);
 
   return (
-    <div className="bg-gray-100 h-auto py-16 flex flex-col items-center justify-center overflow-hidden">
+    <div className="bg-gray-100 h-auto py-10 md:py-16 flex flex-col items-center justify-center overflow-hidden">
       {/* First div for heading */}
       <div className="w-full text-center mb-8">
-        <h2 className="text-gray-800 sm:text-2xl md:text-3xl lg:text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-teal-500 to-purple-600">
+        <h2 className="text-3xl lg:text-4xl text-center md:text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-teal-500 to-purple-600">
           Technologies We Love
         </h2>
       </div>
 
       {/* Wrapper div for technology icons */}
-      <div className="w-full py-16 flex justify-center">
+      <div className="w-full flex justify-center">
         {/* Second div for technologies icons */}
         <div
           ref={containerRef}
-          className="flex items-center space-x-6 sm:space-x-8 md:space-x-12 lg:space-x-16"
+          className="flex items-center space-x-8 md:space-x-12 lg:space-x-16"
         >
           {technologies.map((tech, index) => (
             <TechIcon key={index} icon={tech.icon} name={tech.name} />

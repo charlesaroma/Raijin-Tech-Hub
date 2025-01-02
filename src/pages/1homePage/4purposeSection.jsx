4ee66e859e1388a410a9941c1ac12fde2de62253
Charles Aroma <charlesaroma9@gmail.com>
2025-01-02 23:25:36 +0300

    padding

--- src/pages/1homePage/4purposeSection.jsx
+++ src/pages/1homePage/4purposeSection.jsx
@@ -58,7 +58,7 @@ const PurposeSection = () => {
         </motion.h2>
 
         {/* Responsive Grid */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-12">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 py-10 mt-12">
           {values.map((item, index) => (
             <motion.div
               key={index}

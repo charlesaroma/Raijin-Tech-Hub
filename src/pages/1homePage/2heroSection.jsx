3ae8da876c3eb61ca2459149254ceb3bb340bf63
Charles Aroma <charlesaroma9@gmail.com>
2025-10-18 22:31:24 +0300

    hero update

--- src/pages/1homePage/2heroSection.jsx
+++ src/pages/1homePage/2heroSection.jsx
@@ -124,7 +124,7 @@ const HeroSection = () => {
             className="flex flex-col sm:flex-row gap-4 items-center lg:items-start justify-center lg:justify-start"
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1, delay: 0.8 }}
+            transition={{ duration: 1, delay: 1.3 }}
           >
             <Link
               to="/discover"
